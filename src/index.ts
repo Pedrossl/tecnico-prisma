3caa98b7f31f694936804661903e86c9852c83ac
pedrossl <pedrosslobato@gmail.com>
2025-08-29 15:13:24 -0300

    enable food routes and orders on the server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import personRouter from "./routes/personRoutes.ts";
+import foodRoutes from "./routes/foodRoutes.ts";
+import orderRoutes from "./routes/orderRoutes.ts";
 
 const app = express();
 app.use(express.json());
 
 app.use("/persons", personRouter);
-// app.use("/foods", foodRoutes);
-// app.use("/orders", orderRoutes);
+app.use("/foods", foodRoutes);
+app.use("/orders", orderRoutes);
 
 app.listen(3000, () => console.log("🚀 API running at http://localhost:3000"));
